39963f92af9cc8b9fa95f5c14cad71a8b13098c9
jianleizhang <jianleizhang@Ctrip.com>
2016-08-01 16:12:58 +0800

    more thread

---
+++ test05/src/main/java/com/junsh/test01/TestBlockingQueues.java
@@ -0,0 +1,71 @@
+package com.junsh.test01;
+
+import com.junsh.test.LiftOff;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Created by jianleizhang on 2016/8/1.
+ */
+class LiftOffRunner implements Runnable{
+    private BlockingQueue<LiftOff> rockets;
+    public LiftOffRunner(BlockingQueue<LiftOff> queue){
+        rockets=queue;
+    }
+    public void add(LiftOff lo){
+        try{
+            rockets.put(lo);
+        }catch (InterruptedException e){
+            System.out.println("Interrupted during put().");
+        }
+    }
+    public void run(){
+        try{
+            while (!Thread.interrupted()){
+                LiftOff rocket=rockets.take();
+                rocket.run();
+            }
+        }catch (InterruptedException e){
+            System.out.println("Waking from take().");
+        }
+        System.out.println("Exiting LiftOffRunner.");
+    }
+
+}
+public class TestBlockingQueues {
+    static void getKey(){
+        try{
+            new BufferedReader(new InputStreamReader(System.in)).readLine();
+        }catch (java.io.IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+    static void getKey(String message){
+        System.out.println(message);
+        getKey();
+    }
+    static void test(String msg,BlockingQueue<LiftOff> queue){
+        System.out.println(msg);
+        LiftOffRunner runner=new LiftOffRunner(queue);
+        Thread t=new Thread(runner);
+        t.start();
+        for(int i=0;i<5;i++){
+            runner.add(new LiftOff(5));
+        }
+        getKey("Press 'Enter' ("+msg+")");
+        t.interrupt();
+        System.out.println("Finished "+msg+" test.");
+    }
+
+    public static void main(String[] args) {
+        test("LinkedBlockingQueue",new LinkedBlockingDeque<LiftOff>());
+        test("ArrayBlockingQueue",new ArrayBlockingQueue<LiftOff>(12));
+        test("SynchronousQueue",new SynchronousQueue<LiftOff>());
+    }
+
+}
